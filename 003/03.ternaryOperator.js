356d3975026c141c48a90e38a6c9a13da52b7b1a
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-20 12:11:24 -0300

    ternary

---
+++ 003/03.ternaryOperator.js
@@ -0,0 +1,15 @@
+
+/*
+    * Ternary operator
+
+    it's a shorthand for if-else
+
+    (condition) ? true : false
+*/
+
+let speed = 75;
+const warning = 65;
+
+let message = (speed <= warning) ? 'ok' : 'slow down';
+
+console.log(message)
