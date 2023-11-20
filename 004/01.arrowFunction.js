909f9a2e0397de29448caf3ebbba61cdb8012afc
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-20 14:59:58 -0300

    arrow functions basics

---
+++ 004/01.arrowFunction.js
@@ -0,0 +1,16 @@
+
+/* 
+    * Arrow Function
+    • It's a function expression with a shorter syntax
+*/
+
+const myFunction = () => {
+    console.log('Hello, Arrow Function!');
+}
+
+myFunction();
+
+let x=5
+const double = (x) => x * 2;
+
+console.log(double(x))
