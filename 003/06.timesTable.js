6b1a1b7b12fe3ab7f8d1237274fb4918fd83d4e8
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-20 14:20:27 -0300

    times table

---
+++ 003/06.timesTable.js
@@ -0,0 +1,12 @@
+
+/*
+    * multiplier x number = result
+
+    using for loops
+*/
+
+const multiplier = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${multiplier} x ${i} = ${multiplier * i}`);
+}
