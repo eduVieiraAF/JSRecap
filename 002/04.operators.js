83034d25bb8e402d1fb16fc90e499adf050433ff
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-19 22:11:43 -0300

    operators - arithmetic

---
+++ 002/04.operators.js
@@ -0,0 +1,19 @@
+
+// * Operators: 
+
+// * 1. Arithmetic
+
+let a = 10;
+let b = 5;
+let c = a + b;
+
+console.log(c);
+
+c = a - b;
+console.log(c);
+
+c = a * b;
+console.log(c);
+
+c = a / b;
+console.log(c);
