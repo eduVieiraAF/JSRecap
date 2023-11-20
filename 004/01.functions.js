ed444fe5756a294ee17a8b1d0bd5f9dcdbd9f2d2
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-20 14:54:12 -0300

    function basics

---
+++ 004/01.functions.js
@@ -0,0 +1,27 @@
+
+/* 
+    * A function: 
+    • is a block of code that performs a specific task;
+    • it can be called multiple times;
+*/
+
+// * Function declaration
+function myFunction() {
+    let a = 10;
+    let b = 20;
+    const sum = a + b;
+
+    console.log('Hello World!');
+
+    if (sum < 100) console.log(sum);
+}
+
+// * Function call
+myFunction();
+
+let _name = 'Edu';
+
+if (_name === 'Edu') myFunction();
+else console.log('Hello, stranger!');
+    
+    
