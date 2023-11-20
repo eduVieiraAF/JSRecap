b2cb4d6414d0b31df80469c312a1fdcede16b6d8
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-20 11:58:41 -0300

    if else

---
+++ 003/01.if-else.js
@@ -0,0 +1,18 @@
+
+
+let sum = 14;
+
+// if (sum >= 0) {
+//     console.log("positive");
+// } else {
+//     console.log("negative");
+// }
+
+if (sum > 0) {
+    console.log('positive');
+} else if (sum < 0) {
+    console.log('negative');
+} else {
+    console.log('zero');
+}
+
