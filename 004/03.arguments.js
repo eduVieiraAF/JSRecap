5bec2128668781be2633dca9c68d140b32933a22
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-21 09:11:52 -0300

    args

--- 004/03.arguments.js
+++ 004/03.arguments.js
@@ -16,8 +16,16 @@ function sum (a, b) {
 console.log(sum(10, 40));
 console.log(sum(sum(10, 25), sum(40, 35))) // a function can be passed as an argument
 
-function minus() {
-    return arguments
+// function minus() {
+//     return arguments
+// }
+
+// console.log(minus(1, true, "string"))
+
+function minus(a = 0, b = 0) {
+    if (typeof a !== 'number' && typeof b !== 'number')  return "Not a number"
+
+    return a - b
 }
 
-console.log(minus(1, true, "string"))
+console.log(minus(10, 40));
