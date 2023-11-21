028868e48840ae0834bbb6e4bd74a7d59d59ffea
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-21 10:07:56 -0300

    use strict

---
+++ 004/06.useStrict.js
@@ -0,0 +1,17 @@
+/*
+    * Use Strict
+    • eliminnates silent errors;
+    • prevents unwanted behavior;
+    • optimizes code;
+*/
+
+'use strict';
+
+// let arguments;
+
+(() => {
+    test = 'test';
+
+})();
+
+console.log(test); // will cause error
