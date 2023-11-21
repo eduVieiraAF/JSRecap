e066ae60af6a72f01d9be766612306aefacce0d1
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-21 09:08:50 -0300

    arguments

---
+++ 004/03.arguments.js
@@ -0,0 +1,23 @@
+
+/*
+    * Arguments are values passed to a function also known as parameters
+
+    functionName(arg1, arg2, arg3) {
+        
+    }
+*/
+
+function sum (a, b) {
+    if (typeof a !== 'number' && typeof b !== 'number')  return "Not a number"
+    
+    return a + b;
+}
+
+console.log(sum(10, 40));
+console.log(sum(sum(10, 25), sum(40, 35))) // a function can be passed as an argument
+
+function minus() {
+    return arguments
+}
+
+console.log(minus(1, true, "string"))
