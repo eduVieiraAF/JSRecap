c2ecbd773efc3a719c2ae74f24fccf72975c2ccb
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-26 16:01:29 -0300

    textContext && innerHTML

---
+++ 008/index.js
@@ -0,0 +1,9 @@
+
+
+const p = document.querySelector('p')
+
+console.log(p.textContent)
+console.log(p.innerHTML)
+
+// p.textContent = 'New <strong>text</strong> content'
+p.innerHTML = 'New <strong>text</strong> content'
