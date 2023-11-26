84b19189b73ad1883936f1c89f8121ed9cea93f4
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-25 22:31:16 -0300

    destructuring

---
+++ 006/04.destructuring.js
@@ -0,0 +1,16 @@
+
+/*
+    * Destructuring Arrays
+    it is used asssigning values from arrays or without creating an array
+*/
+
+const [a, b, c, d, e] = [
+    'bread',
+    'milk',
+    'eggs',
+    'cheese',
+    'butter',
+]
+
+console.log(a) // bread
+console.log(b) // milk
