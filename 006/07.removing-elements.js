9760d16f960062dee332d7ec72838af6c599ce94
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-25 22:50:20 -0300

    removing an element from arrays

---
+++ 006/07.removing-elements.js
@@ -0,0 +1,9 @@
+
+
+let arr = [1, 2, "tree", false]
+console.table(arr)
+
+arr.shift() // removes the first element of the array
+arr.pop() // removes the last element of the array
+
+console.table(arr)
