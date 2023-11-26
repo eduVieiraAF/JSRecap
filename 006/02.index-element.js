080796a6781bf723ca7c8ba2d79c9ee88a84f8cc
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-25 22:18:00 -0300

    index and elements

---
+++ 006/02.index-element.js
@@ -0,0 +1,12 @@
+
+/*
+    * Index always starts at 0
+*/
+
+const arr = ["Edu", 41, true, "Dev"]
+
+console.table(arr) 
+console.log(arr[0]) // Edu
+console.log(arr[3]) // Dev
+console.log(arr.length) // 4
+console.info(arr) // [ 'Edu', 41, true, 'Dev' ]
