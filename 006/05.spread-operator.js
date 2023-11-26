0c9fd88d82ceb13c46f9e399a07c5441a96900c4
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-25 22:39:16 -0300

    basics on spread operator

---
+++ 006/05.spread-operator.js
@@ -0,0 +1,7 @@
+
+const num = [1, 2, 4, 8, 16, 32]
+
+console.log(num)
+console.log(...num)
+console.log(Math.max(...num))
+// console.log(Math.max(num)) // NaN as num is an array and Math.max only takes numbers
