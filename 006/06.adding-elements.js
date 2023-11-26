7c802ce46c4730a4a90c7e6e0472f451237785d7
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-25 22:46:11 -0300

    adding to an array

---
+++ 006/06.adding-elements.js
@@ -0,0 +1,10 @@
+
+
+let arr = [1, 2, 3, 4, 5]
+
+arr[2] = 6 // changing a value
+arr[5] = 7 // adding a value/element
+arr.push("push") // adding a value/element at the end of the array
+arr.unshift("unshift") // adding a value/element at the beginning of the array
+
+console.table(arr)
