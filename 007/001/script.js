a4421e6f82c17231d1cacb23846e9625b133ad34
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-26 12:55:05 -0300

    changes

--- 007/001/script.js
+++ 007/001/script.js
@@ -5,7 +5,7 @@ const h1 = document.querySelector('h1')
 setTimeout(() => {
     h1.textContent = 'DOM 101'
     h1.style.color = 'crimson'
-    h1.style.fontSize = '5rem'
+    h1.style.fontSize = 'rem'
     h1.style.backgroundColor = 'black'
 }, 3000)
 
