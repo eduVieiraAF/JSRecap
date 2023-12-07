d2ebfeb4eace07f0ffc1ac43fac5f341ab0f57c3
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-07 14:55:00 -0300

    more on 101

--- 016/script.js
+++ 016/script.js
@@ -2,10 +2,12 @@
 
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', () => {
-    alert('clicked');
-})
+// btn.addEventListener('click', () => {
+//     alert('clicked');
+// })
 
 // function clickHandler() {
 //     alert('clicked');
 // }
+
+// btn.addEventListener('click', clickHandler);
