d82fa37fb53ae43e57509f13aa7c02126eb07055
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-07 14:51:16 -0300

    events 101

---
+++ 016/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    alert('clicked');
+})
+
+// function clickHandler() {
+//     alert('clicked');
+// }
