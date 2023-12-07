db495d5981dd7c8717eb9e424fc3a2311d908f79
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-07 08:50:04 -0300

    classList

--- 014/script.js
+++ 014/script.js
@@ -1,7 +1,7 @@
 
 const h1 = document.querySelector("h1")
 
-h1.style.color = "crimson"
+// h1.style.color = "crimson"
 h1.style.backgroundColor = "black"
 h1.style.fontFamily = "Arial"
 h1.style.padding = "10px"
@@ -14,3 +14,17 @@ p.forEach((it) => {
     it.style.textAlign = "center"
     it.style.color = "seagreen"
 })
+
+// ClassList
+
+setTimeout(() => {
+    h1.classList.add("active", "inverted")
+},3000)
+
+setTimeout(() => {
+    // h1.classList.remove( "inverted")
+    if (h1.classList.contains("inverted")) {
+        h1.classList.toggle("inverted")
+        h1.style.boxShadow = "10px 10px 10px gray"
+    }
+}, 6000)
