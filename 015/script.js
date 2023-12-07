dd2414f81d3b88ad4c972f260d20826206694497
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-07 09:00:52 -0300

    fnding parentElement parentNode

---
+++ 015/script.js
@@ -0,0 +1,9 @@
+
+const strong = document.querySelector('strong')
+const p = document.querySelector('.paragraph')
+console.log(strong.parentElement)
+console.log(strong.parentNode)
+console.log(p.parentNode)
+
+
+// console.log(document.documentElement.parentNode)
