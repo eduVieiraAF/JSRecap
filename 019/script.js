26d05cdffecb0b8f34d023c323568a85b81fc60c
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-12-08 07:57:55 -0300

    formData

--- 019/script.js
+++ 019/script.js
@@ -8,5 +8,32 @@ form.addEventListener('submit', (event) => {
     const username = form.username.value
     const password = form.password.value
 
-    console.log(username, password)
+
+    // if (username && password) {
+    //     console.log(username, password)
+
+    //     form.submit()
+    //     form.reset()
+    // }
+
+    // const formData = new FormData(form)
+    const formData = new FormData()
+
+    formData.set('username', username)
+    formData.set('password', password)
+
+    if (formData.has('username') && formData.has('password')) {
+        formData.forEach(res => {
+            console.log(res)
+            // console.log(formData.get('username'))
+        })
+    }
+
+    form.reset()
+    deleteForm(formData)
 })
+
+function deleteForm(formData) {
+    formData.delete('username')
+    formData.delete('password')
+}
